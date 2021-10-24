13003570e8bd3ba6e6ec934049a32932c8bdbda6
mahkis <mahkis@yandex.ru>
2021-10-24 16:36:39 +0500

    First commit. Create repository.

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('sign-in');
+});
+
+//Route::get('/about', function () {
+//    return view('about');
+//});
+//
+//Route::get('/user', function () {
+//    return view('user');
+//});
+//
+// Route::get('/user/{id}/{name}', function ($id,$name) {
+//    return 'ID:'.$id.'. Name: '.$name;
+//});
