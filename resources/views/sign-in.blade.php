413327e54dd167a1e64bb3c3b8f10d758b5a238c
Murodjon <egamow@gmail.com>
2021-10-24 18:12:07 +0500

    Бошланди

---
+++ resources/views/sign-in.blade.php
@@ -0,0 +1,70 @@
+﻿<!doctype html>
+<html class="no-js " lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
+
+<title>:: APTS :: Тизимга кириш</title>
+<!-- Favicon-->
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+
+<!-- Custom Css -->
+<link rel="stylesheet" href="assets/css/main.css">    
+<link rel="stylesheet" href="assets/css/color_skins.css">
+</head>
+<body class="theme-black">
+<div class="authentication">
+    <div class="container">
+        <div class="col-md-12 content-center">
+            <div class="row">
+                <div class="col-lg-6 col-md-12">
+                    <div class="company_detail">
+                        <h4 class="logo"><img style="width: 30px" src="assets/images/logo-white.png" alt=""> APTS 1.0</h4>
+                        <h3>Автоматлаштирилган кадрлар тайёрлаш тизими</h3>
+                        <p>Ушбу тизим "Фарғонаазот" АЖ талаби асосида <br> "Узбек Холдинг" МЧЖ махсус ишлаб чиқилган </p>
+                        <div class="footer">
+                            <ul  class="social_link list-unstyled">
+                                <li><a href="https://azot.uz" title="Farg'onazot"><i class="zmdi zmdi-globe"></i></a></li>
+                            </ul>
+                            <hr>
+                            <ul>
+                                <li><a href="http://azot.uz" target="_blank">Фарғонаазот</a></li>
+                                <li><a href="http://holding.uz" target="_blank">Узбек-Холдинг</a></li>
+
+                            </ul>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="col-lg-5 col-md-12 offset-lg-1">
+                    <div class="card-plain">
+                        <div class="header">
+                            <h5>Тизимга кириш</h5>
+                        </div>
+                        <form class="form">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Фойдаланувчи табел рақами">
+                                <span class="input-group-addon"><i class="zmdi zmdi-account-circle"></i></span>
+                            </div>
+                            <div class="input-group">
+                                <input type="password" placeholder="Калит сўз (парол)" class="form-control" />
+                                <span class="input-group-addon"><i class="zmdi zmdi-lock"></i></span>
+                            </div>                            
+                        </form>
+                        <div class="footer">
+                            <a href="index" class="btn btn-primary btn-round btn-block">Кириш</a>
+                        </div>
+                        <a href="forgot" class="link">Паролни унутдингизми?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Jquery Core Js -->
+<script src="assets/bundles/libscripts.bundle.js"></script>
+<script src="assets/bundles/vendorscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js -->
+</body>
+</html>
