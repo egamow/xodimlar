1986cd63f7e6eda6142ee74f73e47be42942ff98
Murodjon <egamow@gmail.com>
2022-03-26 10:30:18 +0500

    rrr

---
+++ database/migrations/2021_12_21_122839_add_column_to_user_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('user')->default(true);
+            $table->boolean('admin')->default(false);
+            $table->boolean('trainer')->default(false);
+            $table->boolean('inspector')->default(false);
+            $table->boolean('personnel_officer')->default(false);
+            $table->boolean('curator')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user');
+            $table->dropColumn('admin');
+            $table->dropColumn('trainer');
+            $table->dropColumn('inspector');
+            $table->dropColumn('personnel_officer');
+            $table->dropColumn('curator');
+
+        });
+    }
+}
